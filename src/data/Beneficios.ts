7d8e4a1e8ce833104b8b1db40a9b5217dfb915b6
Luis Enrique <enriquecalero284@gmail.com>
2024-06-18 12:58:43 -0500

    pagina 404 lista, beneficio

---
+++ src/data/Beneficios.ts
@@ -0,0 +1,20 @@
+const Beneficios = [
+    {
+        icon: 'moodle',
+        title: 'Precisión Garantizada Siempre'
+    },
+    {
+        icon: 'moodle',
+        title: 'Tecnología Avanzada'
+    },
+    {
+        icon: 'moodle',
+        title: 'Soporte Técnico Disponible'
+    },
+    {
+        icon: 'moodle',
+        title: 'Alta Durabilidad Asegurada'
+    },
+]
+
+export default Beneficios;
